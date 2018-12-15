8aaaa34639dac86a802319be461cb7e4e35a348f
Richard Cook <rcook@rcook.org>
2018-12-15 07:29:18 -0800

    Improve validation

--- src/MazesForProgrammers/Funcs.hs
+++ src/MazesForProgrammers/Funcs.hs
@@ -12,10 +12,10 @@ maze m n = Maze m n Map.empty
 
 updateSideMap :: Int -> Int -> Map Loc [Side] -> Opening -> Maybe (Map Loc [Side])
 updateSideMap m n mp (Opening loc@(i, j) side)
-    | side == N && i > 0 = Just (Map.insertWith (++) (i - 1, j) [S] mp')
-    | side == E && j < n - 1 = Just (Map.insertWith (++) (i, j + 1) [W] mp')
-    | side == S && i < m - 1 = Just (Map.insertWith (++) (i + 1, j) [N] mp')
-    | side == W && j > 0 = Just (Map.insertWith (++) (i, j - 1) [E] mp')
+    | side == N && i > 0 && i < m = Just (Map.insertWith (++) (i - 1, j) [S] mp')
+    | side == E && j >= 0 && j < n - 1 = Just (Map.insertWith (++) (i, j + 1) [W] mp')
+    | side == S && i >= 0 && i < m - 1 = Just (Map.insertWith (++) (i + 1, j) [N] mp')
+    | side == W && j > 0 && j < n = Just (Map.insertWith (++) (i, j - 1) [E] mp')
     | otherwise = Nothing
     where mp' =  Map.insertWith (++) loc [side] mp
 
