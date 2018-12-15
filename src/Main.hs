743435b7aa03a933b407577dda5235f713410e9e
Richard Cook <rcook@rcook.org>
2018-12-15 13:54:17 -0800

    Implement binary tree algorithm in IO

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
+import           Data.Bool
 import           Data.Foldable
 import           MazesForProgrammers
 import           System.IO.Temp
+import           System.Random
 
 pngTempFileTemplate :: FilePath
 pngTempFileTemplate = "mazes-for-programmers-.png"
 
-main :: IO ()
-main = do
+demoHandwrittenMaze :: IO ()
+demoHandwrittenMaze = do
     let mbMaze = foldlM withOpening (maze 5 4)
             [ Opening (0, 0) E
             , Opening (0, 1) E
@@ -25,3 +27,36 @@ main = do
     p <- emptySystemTempFile pngTempFileTemplate
     mazePng p mz
     putStrLn "Done"
+
+demoBinaryTreeMaze :: IO ()
+demoBinaryTreeMaze = do
+    let mz@(Maze m n _) = maze 5 4
+
+    -- This is the binary tree algorithm
+    -- It all runs in IO which sucks
+    -- I'll fix this later
+    openings <- foldlM (\os (i, j) -> do
+        let ns0 = []
+            ns1 = bool ns0 (Opening (i, j) N : ns0) (i > 0)
+            ns2 = bool ns1 (Opening (i, j) W : ns1) (j > 0)
+            count = length ns2
+        if count > 0
+            then do
+                idx <- randomRIO (0, count - 1)
+                let neighbour = ns2 !! idx
+                pure $ neighbour : os
+            else pure os)
+        []
+        [(i, j) | i <- [0..(m - 1)], j <- [0..(n - 1)]]
+
+    let mbResult = foldlM withOpening mz openings
+        result = case mbResult of
+            Nothing -> error "Invalid maze"
+            Just mz' -> mz'
+
+    p <- emptySystemTempFile pngTempFileTemplate
+    mazePng p result
+    putStrLn "Done"
+
+main :: IO ()
+main = demoBinaryTreeMaze
